0c44a3bc7ac9735b325f2c56683a8dc5ae8d1369
vortex <ffvortex20@gmail.com>
2026-01-06 19:25:08 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-const firebaseConfig = {
+export const app = initializeApp({
   apiKey: "AIzaSyAx-jIvw9lqykjroKMWzRTEOlZ41Xk6-bY",
   authDomain: "vortexxxxxxxxx-5159f.firebaseapp.com",
   databaseURL: "https://vortexxxxxxxxx-5159f-default-rtdb.firebaseio.com",
   projectId: "vortexxxxxxxxx-5159f",
-  storageBucket: "vortexxxxxxxxx-5159f.firebasestorage.app",
-  messagingSenderId: "921732735688",
-  appId: "1:921732735688:web:vortex"
-};
+  storageBucket: "vortexxxxxxxxx-5159f.firebasestorage.app"
+});
 
-export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const storage = getStorage(app);
+</script>
