268c89aa2abfff2beef898cf04c95370b0cfc9c1
vortex <ffvortex20@gmail.com>
2026-01-06 19:15:39 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAx-jIvw9lqykjroKMWzRTEOlZ41Xk6-bY",
   authDomain: "vortexxxxxxxxx-5159f.firebaseapp.com",
   databaseURL: "https://vortexxxxxxxxx-5159f-default-rtdb.firebaseio.com",
@@ -16,4 +15,3 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const storage = getStorage(app);
-</script>
